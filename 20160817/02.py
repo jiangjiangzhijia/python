d79e6ca3648105870f793e3e71cffa8209767422
jinagjiangzhijia <1327656107@qq.com>
2016-08-17 23:38:34 +0800

    02

---
+++ 20160817/02.py
@@ -0,0 +1,31 @@
+#1
+#name = raw_input("what's your name : ")
+#print 'hello,'+name
+
+#2
+#print 'let\'s go'
+#print repr('hello world')
+#print str('hello world')
+
+#print `hello world`
+#3
+#temp = 42
+#print "the Temperature is " + `temp`
+#print "the Temperature is " + repr(temp)
+#print "the Temperature is " + str(temp)
+#4
+#name = raw_input("What's your name?")
+#print 'Hello. ' + name + "!"
+
+#5
+#print '''asdfsadfasdfasdfasdfasdfasdfasdfasdfsadfsadfasdfsdjasdjfklsjdfkjsdfkljdskfjksdfjkjfkjsdkfjksdfjkdjfskjfkdsjf
+#sdfsdfsadfsdf'''
+
+#6
+#print r"hello,world" '\\'
+
+#7
+#print u'Hello,world'
+help()
+raw_input("Press <enter>")
+
